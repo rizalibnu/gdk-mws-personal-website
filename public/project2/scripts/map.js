0551332424c31ff4512a19db820ab3575269d56b
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-10-26 01:36:07 +0700

    Update project map

--- public/project2/scripts/map.js
+++ public/project2/scripts/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([-6.175427, 106.827153], 15);
+var mymap = L.map('mapid').setView([-6.175427, 106.827153], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
