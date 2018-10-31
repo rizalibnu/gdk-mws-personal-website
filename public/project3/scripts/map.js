e03f8f17bdcfcd31ec56b886dbad6adb457d5ddd
Rizal Ibnu <muhamadrizalibnuabdulah@gmail.com>
2018-11-01 01:40:11 +0700

    Update map project 3

--- public/project3/scripts/map.js
+++ public/project3/scripts/map.js
@@ -16,45 +16,51 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   } catch (err) {
     console.log(err);
   }
+
+  const img = document.getElementById('image');
+  const title = document.getElementById('title');
+  const review = document.getElementById('review');
+
+  let places;
+  try {
+    places = await JSON.parse(await localStorage.getItem('places'));
+  } catch (err) {
+    console.log(err);
+  }
+
+  const findLocation = (x, y) => {
+    for (let i = 0; i < places.length; i++) {
+      if (places[i].location[0] === x &&
+        places[i].location[1] === y) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  const insertView = (index) => {
+    img.src = places[index].image;
+    img.alt = places[index].title;
+    title.innerHTML = places[index].title;
+    review.innerHTML = places[index].review;
+  }
+
+  const showLocation = (e) => {
+    const index = findLocation(e.latlng.lat, e.latlng.lng);
+    if (index >= 0) {
+      insertView(index);
+    }
+  }
+
+  if (places) {
+    places.map((place, index) => {
+      var marker = L.marker(place.location).addTo(mymap).bindPopup(`<b>${place.title}</b><br />${place.shortReview}`);
+      marker.on('click', showLocation);
+      if (index === 0) {
+        marker.openPopup();
+      }
+    })
+    // Init data
+    insertView(0);
+  }
 })();
-
-const img = document.getElementById('image');
-const title = document.getElementById('title');
-const review = document.getElementById('review');
-const places = JSON.parse(localStorage.getItem('places'));
-
-const findLocation = (x, y) => {
-  for (let i = 0; i < places.length; i++) {
-    if (places[i].location[0] === x &&
-      places[i].location[1] === y) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-const insertView = (index) => {
-  img.src = places[index].image;
-  img.alt = places[index].title;
-  title.innerHTML = places[index].title;
-  review.innerHTML = places[index].review;
-}
-
-const showLocation = (e) => {
-  const index = findLocation(e.latlng.lat, e.latlng.lng);
-  if (index >= 0) {
-    insertView(index);
-  }
-}
-
-if (places) {
-  places.map((p, index) => {
-    var marker = L.marker(p.location).addTo(mymap).bindPopup(`<b>${p.title}</b><br />${p.shortReview}`);
-    marker.on('click', showLocation);
-    if (index === 0) {
-      marker.openPopup();
-    }
-  })
-  // Init data
-  insertView(0);
-}
